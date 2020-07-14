fb22e51ba5ddc775904aa62a76133ccb00609278
johnathantng <johnathan.tng@gmail.com>
2020-07-13 21:31:36 -0400

    save progress 7/13

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
+
+    int max = 1000;
+    int min = 1;
+    int guess = 500;
+
     // Start is called before the first frame update
     void Start()
     {
-        int max = 1000;
-        int min = 1;
 
         Debug.Log("Welcome to the number wizard, yo");
         Debug.Log("Pick a number");
